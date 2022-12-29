549511e565e0ec1fe003d837906488e6dc2e9db6
Brandon Apol <brandonapol@cedarville.edu>
2022-12-28 21:47:22 -0500

    initial commit

---
+++ 1-1.scala
@@ -0,0 +1,15 @@
+// def add(a: Int, b: Int): Int = {
+//     a + b
+// }
+
+def increment(x: Int): Int = {
+    x + 1
+}
+
+def getFirstCharacter(s: String): Char = {
+    s.charAt(0);
+}
+
+def wordScore(word: String): Int = {
+    word.length()
+}
